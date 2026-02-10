5df085df841bc682a97e713a68ebdb008eaf0330
Sebastian Muszynski <basti@linkt.de>
2026-02-10 08:28:54 +0100

    Fix

--- components/tianpower_bms_ble/tianpower_bms_ble.cpp
+++ components/tianpower_bms_ble/tianpower_bms_ble.cpp
@@ -222,7 +222,6 @@ void TianpowerBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_i
 
 void TianpowerBmsBle::update() {
   this->track_online_status_();
-
   if (this->node_state != espbt::ClientState::ESTABLISHED) {
     ESP_LOGW(TAG, "[%s] Not connected", ADDR_STR(this->parent_->address_str()));
     return;
