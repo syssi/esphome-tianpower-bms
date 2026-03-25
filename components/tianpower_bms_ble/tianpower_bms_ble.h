6383e8a2f4b7ca6b844604b5d8c3721ec81e23e1
Sebastian Muszynski <basti@linkt.de>
2026-03-25 09:12:39 +0100

    Explicitly initialize all pointer members to nullptr

--- components/tianpower_bms_ble/tianpower_bms_ble.h
+++ components/tianpower_bms_ble/tianpower_bms_ble.h
@@ -137,46 +137,46 @@ class TianpowerBmsBle : public esphome::ble_client::BLEClientNode, public Pollin
   void on_tianpower_bms_ble_data(const uint8_t &handle, const std::vector<uint8_t> &data);
 
  protected:
-  binary_sensor::BinarySensor *charging_binary_sensor_;
-  binary_sensor::BinarySensor *discharging_binary_sensor_;
-  binary_sensor::BinarySensor *limiting_current_binary_sensor_;
-  binary_sensor::BinarySensor *balancing_binary_sensor_;
-  binary_sensor::BinarySensor *online_status_binary_sensor_;
+  binary_sensor::BinarySensor *charging_binary_sensor_{nullptr};
+  binary_sensor::BinarySensor *discharging_binary_sensor_{nullptr};
+  binary_sensor::BinarySensor *limiting_current_binary_sensor_{nullptr};
+  binary_sensor::BinarySensor *balancing_binary_sensor_{nullptr};
+  binary_sensor::BinarySensor *online_status_binary_sensor_{nullptr};
 
   uint8_t no_response_count_{0};
 
-  sensor::Sensor *total_voltage_sensor_;
-  sensor::Sensor *current_sensor_;
-  sensor::Sensor *power_sensor_;
-  sensor::Sensor *charging_power_sensor_;
-  sensor::Sensor *discharging_power_sensor_;
-  sensor::Sensor *capacity_remaining_sensor_;
-  sensor::Sensor *voltage_protection_bitmask_sensor_;
-  sensor::Sensor *current_protection_bitmask_sensor_;
-  sensor::Sensor *temperature_protection_bitmask_sensor_;
-  sensor::Sensor *error_bitmask_sensor_;
-  sensor::Sensor *state_of_charge_sensor_;
-  sensor::Sensor *nominal_capacity_sensor_;
-  sensor::Sensor *charging_cycles_sensor_;
-  sensor::Sensor *min_cell_voltage_sensor_;
-  sensor::Sensor *max_cell_voltage_sensor_;
-  sensor::Sensor *min_voltage_cell_sensor_;
-  sensor::Sensor *max_voltage_cell_sensor_;
-  sensor::Sensor *delta_cell_voltage_sensor_;
-  sensor::Sensor *average_cell_voltage_sensor_;
-  sensor::Sensor *average_temperature_sensor_;
-  sensor::Sensor *ambient_temperature_sensor_;
-  sensor::Sensor *mosfet_temperature_sensor_;
-  sensor::Sensor *state_of_health_sensor_;
-  sensor::Sensor *balancing_bitmask_sensor_;
-  sensor::Sensor *alarm_bitmask_sensor_;
+  sensor::Sensor *total_voltage_sensor_{nullptr};
+  sensor::Sensor *current_sensor_{nullptr};
+  sensor::Sensor *power_sensor_{nullptr};
+  sensor::Sensor *charging_power_sensor_{nullptr};
+  sensor::Sensor *discharging_power_sensor_{nullptr};
+  sensor::Sensor *capacity_remaining_sensor_{nullptr};
+  sensor::Sensor *voltage_protection_bitmask_sensor_{nullptr};
+  sensor::Sensor *current_protection_bitmask_sensor_{nullptr};
+  sensor::Sensor *temperature_protection_bitmask_sensor_{nullptr};
+  sensor::Sensor *error_bitmask_sensor_{nullptr};
+  sensor::Sensor *state_of_charge_sensor_{nullptr};
+  sensor::Sensor *nominal_capacity_sensor_{nullptr};
+  sensor::Sensor *charging_cycles_sensor_{nullptr};
+  sensor::Sensor *min_cell_voltage_sensor_{nullptr};
+  sensor::Sensor *max_cell_voltage_sensor_{nullptr};
+  sensor::Sensor *min_voltage_cell_sensor_{nullptr};
+  sensor::Sensor *max_voltage_cell_sensor_{nullptr};
+  sensor::Sensor *delta_cell_voltage_sensor_{nullptr};
+  sensor::Sensor *average_cell_voltage_sensor_{nullptr};
+  sensor::Sensor *average_temperature_sensor_{nullptr};
+  sensor::Sensor *ambient_temperature_sensor_{nullptr};
+  sensor::Sensor *mosfet_temperature_sensor_{nullptr};
+  sensor::Sensor *state_of_health_sensor_{nullptr};
+  sensor::Sensor *balancing_bitmask_sensor_{nullptr};
+  sensor::Sensor *alarm_bitmask_sensor_{nullptr};
 
-  text_sensor::TextSensor *software_version_text_sensor_;
-  text_sensor::TextSensor *device_model_text_sensor_;
-  text_sensor::TextSensor *voltage_protection_text_sensor_;
-  text_sensor::TextSensor *current_protection_text_sensor_;
-  text_sensor::TextSensor *temperature_protection_text_sensor_;
-  text_sensor::TextSensor *errors_text_sensor_;
+  text_sensor::TextSensor *software_version_text_sensor_{nullptr};
+  text_sensor::TextSensor *device_model_text_sensor_{nullptr};
+  text_sensor::TextSensor *voltage_protection_text_sensor_{nullptr};
+  text_sensor::TextSensor *current_protection_text_sensor_{nullptr};
+  text_sensor::TextSensor *temperature_protection_text_sensor_{nullptr};
+  text_sensor::TextSensor *errors_text_sensor_{nullptr};
 
   struct Cell {
     sensor::Sensor *cell_voltage_sensor_{nullptr};
